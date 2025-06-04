48a429445a598097c03ba3b773afdf61e34e0c64
Seba <sebacoli00@gmail.com>
2025-06-04 15:31:40 -0300

    Home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,147 @@
+import "./HomePage.css";
+
+function HomePage() {
+
+  return (
+    <>
+    <div className="home-page">
+        <div className="absolute inset-0 bg-opacity-60" />
+
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
+        <h1 className="text-4xl sm:text-6xl font-extrabold uppercase">
+          Sebastian Colina's
+        </h1>
+        <h2 className="text-3xl sm:text-5xl font-bold mt-2 uppercase">
+          Portfolio
+        </h2>
+        <hr className="w-1/2 my-6 border-gray-400" />
+        <p className="text-lg sm:text-xl">Welcome to my website!</p>
+      </div>
+    </div>
+    
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+        esad
+        easd
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+        esad
+        easd
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+        esad
+        easd
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+        esad
+        easd
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+        esad
+        easd
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+    </div>
+    <div>
+        poner mas cosas
+        esad
+        easd
+    </div>
+
+    </>
+  )
+}
+
+export default HomePage
